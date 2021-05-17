d74804fbf5ef593b488b7dec449bba55906ed84d
Yiran Wang <30646360+ywang951@users.noreply.github.com>
2021-05-16 20:41:03 -0400

    Update functions.r

--- functions.r
+++ functions.r
@@ -6,6 +6,36 @@ library(pracma)
 library(igraph)
 library(doBy)
 
+permutation <- function(network, namesequence) {
+  # This function is from the R package "snatm", which is not compatible with my system.
+  if (dim(network)[1] != length(namesequence)) {
+    stop("Dimension of network and length of namesequence must be equal.")
+  }
+  if (length(unique(namesequence)) < dim(network)[1]) {
+    stop("Each name in namesequence must be different.")
+  }
+  newnetwork <- network
+  while (!all(rownames(newnetwork) == namesequence)) {
+    for (i in 1:dim(network)[1]) {
+      if (rownames(network)[i] != namesequence[i]) {
+        columnposition <- which(rownames(network) == namesequence[i])
+        newnetwork[, i] <- network[, columnposition]
+        newnetwork[, columnposition] <- network[, i]
+        tempcolumn <- newnetwork[i, ]
+        newnetwork[i, ] <- newnetwork[columnposition, ]
+        newnetwork[columnposition, ] <- tempcolumn
+        temprowname <- colnames(newnetwork)[i]
+        colnames(newnetwork)[i] <- rownames(network)[columnposition]
+        rownames(newnetwork)[i] <- rownames(network)[columnposition]
+        colnames(newnetwork)[columnposition] <- temprowname
+        rownames(newnetwork)[columnposition] <- temprowname
+        network <- newnetwork
+      }
+    }
+  }
+  newnetwork
+}
+
 Generate.A <- function(P) {
   #' @param P: edge-probability matrices
   #' @return output: adjcency matrices
